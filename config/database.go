c1919189c4fd7c7f2816bd8cedeb154eac00b485
ElImam21 <imulyazid@gmail.com>
2025-12-24 09:00:24 +0700

    tambahkan convert url

--- config/database.go
+++ config/database.go
@@ -3,6 +3,7 @@ package config
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
@@ -12,20 +13,22 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	// Load .env (aman, di Railway akan dilewati)
 	_ = godotenv.Load()
 
-	dsn := os.Getenv("MYSQL_URL")
-	if dsn == "" {
+	rawURL := os.Getenv("MYSQL_URL")
+	if rawURL == "" {
 		log.Fatal("❌ MYSQL_URL tidak ditemukan")
 	}
+
+	// convert mysql://user:pass@host:port/db
+	dsn := strings.TrimPrefix(rawURL, "mysql://") +
+		"?charset=utf8mb4&parseTime=True&loc=Local"
 
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("❌ Gagal koneksi database:", err)
 	}
 
-	// aktifkan proteksi SQL injection
 	registerQueryProtector(database)
 
 	DB = database
